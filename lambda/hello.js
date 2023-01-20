b6c7d115218fa160673978e4bedb0249a0a96e2b
elvisbrevi <elvisbrevi@gmail.com>
2023-01-20 14:35:38 -0300

    second stack with ecr repo creation

--- lambda/hello.js
+++ lambda/hello.js
@@ -3,6 +3,7 @@ exports.handler = async function(event) {
     return {
       statusCode: 200,
       headers: { "Content-Type": "text/plain" },
+      
       body: `Good Night, CDK! You've hit ${event.path}\n`
     };
   };
