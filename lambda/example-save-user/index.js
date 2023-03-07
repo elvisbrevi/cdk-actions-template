f7891077a452805e095aad936e2d2d05798b1c58
elvisbrevi <elvisbrevi@gmail.com>
2023-03-07 08:19:49 -0300

    fix name

--- lambda/example-save-user/index.js
+++ lambda/example-save-user/index.js
@@ -1,10 +1,10 @@
 exports.handler = async function(event) {
-    console.log("StageName is:" + process.env.stage);
-    console.log("request:", JSON.stringify(event, undefined, 2));
-    return {
-      statusCode: 200,
-      headers: { "Content-Type": "text/plain" },
-      
-      body: `save order`
-    };
+  console.log("StageName is:" + process.env.stage);
+  console.log("request:", JSON.stringify(event, undefined, 2));
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+
+    body: `From Expample folder, CDK! You've hit ${event.path}\n`
+  };
 };
