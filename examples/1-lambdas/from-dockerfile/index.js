5ec9bec2fdff870c2d023d0da172f46e34e55662
elvisbrevi <elvisbrevi@gmail.com>
2023-03-09 22:26:41 -0300

    fix lambda example

--- examples/1-lambdas/from-dockerfile/index.js
+++ examples/1-lambdas/from-dockerfile/index.js
@@ -2,6 +2,6 @@ exports.handler = async function(event) {
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
-    body: `Hello World from Dockerfile!`
+    body: `Hello World from Dockerfile.`
   };
 };
